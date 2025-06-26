917d4d1838ef908d755d4444551c2774e6972ceb
Arthur Fontaine <0arthur.fontaine@gmail.com>
2025-06-26 02:34:33 +0200

    fix(telemetry): set pino-loki batching to false to force logs to send when there is not enough logs

--- apps/api/src/logger.ts
+++ apps/api/src/logger.ts
@@ -5,8 +5,7 @@ import packageJson from "../package.json" with { type: "json" };
 const stream = pinoLoki({
 	host: "http://loki:3100",
 	labels: { app: packageJson.name },
-	interval: 5_000, // Send logs every 5 seconds
-	batching: true,
+	batching: false,
 });
 
 export const logger = pino({}, stream);
