c402712529e0741b5a1ad50cf9d90ca99a922a53
Arthur Fontaine <0arthur.fontaine@gmail.com>
2025-07-12 20:15:29 +0200

    refactor(db): :fire: remove table type exports

--- packages/db/src/adapters/mikro-orm/index.ts
+++ packages/db/src/adapters/mikro-orm/index.ts
@@ -10,23 +10,18 @@ export type {
 	IAccount,
 	IInsertAccount,
 	IUpdateAccount,
-	IAccountTable,
 } from "../../domain/models/IAccount.ts";
 
 export type {
 	IApiCredential,
 	IInsertApiCredential,
 	IUpdateApiCredential,
-	IApiCredentialTable,
 } from "../../domain/models/IApiCredential.ts";
-
-export type { IBaseModel } from "../../domain/models/IBaseModel.ts";
 
 export type {
 	ICheckoutPage,
 	IInsertCheckoutPage,
 	IUpdateCheckoutPage,
-	ICheckoutPageTable,
 	ICheckoutDisplayData,
 } from "../../domain/models/ICheckoutPage.ts";
 
@@ -34,84 +29,72 @@ export type {
 	ICheckoutTheme,
 	IInsertCheckoutTheme,
 	IUpdateCheckoutTheme,
-	ICheckoutThemeTable,
 } from "../../domain/models/ICheckoutTheme.ts";
 
 export type {
 	ICompany,
 	IInsertCompany,
 	IUpdateCompany,
-	ICompanyTable,
 } from "../../domain/models/ICompany.ts";
 
 export type {
 	IJwtToken,
 	IInsertJwtToken,
 	IUpdateJwtToken,
-	IJwtTokenTable,
 } from "../../domain/models/IJwtToken.ts";
 
 export type {
 	IOAuth2Client,
 	IInsertOAuth2Client,
 	IUpdateOAuth2Client,
-	IOAuth2ClientTable,
 } from "../../domain/models/IOAuth2Client.ts";
 
 export type {
 	IPaymentAttempt,
 	IInsertPaymentAttempt,
 	IUpdatePaymentAttempt,
-	IPaymentAttemptTable,
 } from "../../domain/models/IPaymentAttempt.ts";
 
 export type {
 	IRefund,
 	IInsertRefund,
 	IUpdateRefund,
-	IRefundTable,
 } from "../../domain/models/IRefund.ts";
 
 export type {
 	ISession,
 	IInsertSession,
 	IUpdateSession,
-	ISessionTable,
 } from "../../domain/models/ISession.ts";
 
 export type {
 	IStoreMember,
 	IInsertStoreMember,
 	IUpdateStoreMember,
-	IStoreMemberTable,
 } from "../../domain/models/IStoreMember.ts";
 
 export type {
 	IStore,
 	IInsertStore,
 	IUpdateStore,
-	IStoreTable,
 } from "../../domain/models/IStore.ts";
 
 export type {
 	IStoreStatus,
 	IInsertStoreStatus,
 	IUpdateStoreStatus,
-	IStoreStatusTable,
 } from "../../domain/models/IStoreStatus.ts";
 
 export type {
 	IThemeCustomization,
 	IInsertThemeCustomization,
 	IUpdateThemeCustomization,
-	IThemeCustomizationTable,
 } from "../../domain/models/IThemeCustomization.ts";
 
 export type {
 	ITransactionEvent,
 	IInsertTransactionEvent,
 	IUpdateTransactionEvent,
-	ITransactionEventTable,
 	ITransactionEventData,
 } from "../../domain/models/ITransactionEvent.ts";
 
@@ -119,28 +102,24 @@ export type {
 	ITransaction,
 	IInsertTransaction,
 	IUpdateTransaction,
-	ITransactionTable,
 } from "../../domain/models/ITransaction.ts";
 
 export type {
 	IUser,
 	IInsertUser,
 	IUpdateUser,
-	IUserTable,
 } from "../../domain/models/IUser.ts";
 
 export type {
 	IUserProfile,
 	IInsertUserProfile,
 	IUpdateUserProfile,
-	IUserProfileTable,
 } from "../../domain/models/IUserProfile.ts";
 
 export type {
 	IVerification,
 	IInsertVerification,
 	IUpdateVerification,
-	IVerificationTable,
 } from "../../domain/models/IVerification.ts";
 
 export const loadModels = () => {
