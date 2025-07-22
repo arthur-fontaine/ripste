4fce351bbd9c077b727edbefffd6e319da6449c9
Thierry Maignan <81976242+thierrymgn@users.noreply.github.com>
2025-07-22 19:58:12 +0200

    fix(models): remove eventType and update eventData type to non-nullable (#73)

--- packages/db/src/domain/models/ITransactionEvent.ts
+++ packages/db/src/domain/models/ITransactionEvent.ts
@@ -5,8 +5,7 @@ import type { ITransactionTable } from "./ITransaction.ts";
 import type { IsoString } from "../../types/iso-string.d.ts";
 
 export interface ITransactionEventTable extends IBaseModel {
-	eventType: string;
-	eventData: ITransactionEventData | null;
+	eventData: ITransactionEventData;
 	transaction: ISU.SingleReference<ITransactionTable, "transactionId", "id">;
 }
 
