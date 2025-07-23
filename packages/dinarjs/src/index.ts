c24e2b6e5515c2e9c41b85d8ab0bcadbfda8dc4f
Arthur Fontaine <0arthur.fontaine@gmail.com>
2025-07-23 10:25:09 +0200

    feat(dinarjs): :zap: return amount early if source and target currencies are the same

--- packages/dinarjs/src/index.ts
+++ packages/dinarjs/src/index.ts
@@ -29,6 +29,8 @@ export class Dinar {
 	}
 
 	async convert(amount: number, from: string, to: string): Promise<number> {
+		if (from === to) return amount;
+
 		const rates = await this.#cache.callWithCache(`rates-${from}`, () =>
 			this.#currencyProvider.getRates(from),
 		);
