c9a39ed6daa010bf358e890904f074ec81cf9993
Arthur Fontaine <0arthur.fontaine@gmail.com>
2025-07-24 13:46:19 +0200

    refactor(db): :fire: do not create user automatically when creating company

--- packages/db/src/adapters/mikro-orm/repositories/MikroOrmCompanyRepository.ts
+++ packages/db/src/adapters/mikro-orm/repositories/MikroOrmCompanyRepository.ts
@@ -6,28 +6,9 @@ import type {
 	IInsertCompany,
 	IUpdateCompany,
 } from "../../../domain/models/ICompany.ts";
-import { MikroOrmUserModel } from "../models/MikroOrmUserModel.ts";
-import type { EntityManager } from "@mikro-orm/core";
 
 export class MikroOrmCompanyRepository
 	extends MikroOrmBaseRepository<ICompany, IInsertCompany, IUpdateCompany>(
 		MikroOrmCompanyModel,
 	)
-	implements ICompanyRepository
-{
-	constructor(options: { em: EntityManager }) {
-		super(options);
-		this.insert = async (entity: IInsertCompany): Promise<ICompany> => {
-			const { userId, ...companyData } = entity;
-			const newCompany = this._em.create(
-				MikroOrmCompanyModel,
-				companyData as never,
-			);
-			const userRef = this._em.getReference(MikroOrmUserModel, userId);
-			newCompany.user = userRef;
-
-			await this._em.persistAndFlush(newCompany);
-			return newCompany;
-		};
-	}
-}
+	implements ICompanyRepository {}
